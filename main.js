df07907adee4ad33926fb4824bed275f2855b7c1
Guylherme Duarte <guylherme_duarte@hotmail.com>
2022-12-14 10:19:33 -0300

    Adicionado abas de skills

--- main.js
+++ main.js
@@ -29,4 +29,31 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 
-/*============================= MOSTRAR E ESCONDER MENU =============================*/
+/*============================= SANFONA SKILLS =============================*/
+const skillsContent = document.getElementsByClassName('skills__content'),
+  skillsHeader = document.querySelectorAll('.skills__header')
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className
+
+  for (i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skills__content skills__close'
+  }
+  if (itemClass === 'skills__content skills__close') {
+    this.parentNode.className = 'skills__content skills__open'
+  }
+}
+
+skillsHeader.forEach(el => {
+  el.addEventListener('click', toggleSkills)
+})
+
+/*============================= QUALIFICATION TABS =========================*/
+
+/*============================= SERVICES MODAL =============================*/
+
+/*============================= PORTIFOLIO SWIPER ===========================*/
+
+/*============================= TESTIMONIAL =================================*/
+
+/*============================= SCROLL SECTIONS ACTIVE LINK ==================*/
